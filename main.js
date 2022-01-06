b7af3199d889ef199f95ede990048a2dba660e79
Legit-Sam <webdeveloper2167@gmail.com>
2022-01-06 22:15:08 +0100

    Update

---
+++ main.js
@@ -0,0 +1,13 @@
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    var cPassword = document.getElementById("c-password").value;
+    var message = document.getElementById("message");
+      if (password == cPassword) {
+        message.textContent = "Passwords Match";
+        message.style.backgroundColor = "green";
+      } else {
+        message.textContent = "Passwords don't Match";
+        message.style.backgroundColor = "red";
+      }
+  }
+  
